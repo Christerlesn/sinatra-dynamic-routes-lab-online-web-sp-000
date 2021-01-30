253af43f4927e0de181396685aba88eccf4d63b4
Christerle Nounoute <christerlesn@gmail.com>
2021-01-30 20:55:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,11 +21,12 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    @op = params[:operation].to_s
+    @op = params[:operation]
     @num = params[:number1].to_i
     @num2 = params[:number2].to_i
-    # test = "#{@num}" "#{@op}" "#{@num2}"
-    "#{@num @op @num2}"
+  when "subtract"
+    (@num - @num2).to_s
+  end
   end
 
 end #end of class
