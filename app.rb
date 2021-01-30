812015aba235b8ba5ce47fc809b5f1c00e9ec4eb
Christerle Nounoute <christerlesn@gmail.com>
2021-01-30 20:57:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ class App < Sinatra::Base
     case @op
       when "subtract"
         (@num - @num2).to_s
-      when"add" OR "addition"
+      when"add" || "addition"
         (@num + @num2)
       end
   end
