5baf748b988b369f630025e1bf24c51fb4980ee4
Christerle Nounoute <christerlesn@gmail.com>
2021-01-30 20:24:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,9 +14,9 @@ class App < Sinatra::Base
     "#{params[:number].to_i * params[:number].to_i}"
   end
 
-  get '/say/:word1' do
-    @word = params[:word1]
-    "#{params[:word1]}"
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1] params[:word2] params[:word3] params[:word4]
+     params[:word5]}"
   end
 
 end #end of class
