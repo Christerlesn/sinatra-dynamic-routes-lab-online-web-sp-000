f8ff22c314bfcc520ee3f803e946b7f4a2a9e229
Christerle Nounoute <christerlesn@gmail.com>
2021-01-30 19:32:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/' do
+    "This is the homepage. Every website needs a homepage."
+  end
   get 'reversename/:name' do
     "#{params[:name].reverse}"
   end
