89b1b0d3d51830d36323d02a8257cf7641ab3e26
Christerle Nounoute <christerlesn@gmail.com>
2021-01-30 19:50:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,9 +16,9 @@ class App < Sinatra::Base
 
   get '/say/:number' do
     @num = params[:number].to_i
-    @num.times do |sentence|
-      print "Sup"
-    end
+    # @num.times do |sentence|
+    #   print "Sup"
+    #end
   end
 
 end #end of class
